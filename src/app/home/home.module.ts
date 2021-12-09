c5273da0830289dcac78362911025cfcdf879793
Alessandro Cigolini <83404657+AleCigolini@users.noreply.github.com>
2021-12-08 21:54:13 -0300

    initial project

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,21 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home.component';
+
+@NgModule({
+  declarations: [
+    HomeComponent
+  ],
+  imports: [
+    CommonModule,
+    HomeRoutingModule,
+    MatToolbarModule,
+    MatIconModule
+  ],
+  exports: [HomeComponent]
+})
+export class HomeModule { }
